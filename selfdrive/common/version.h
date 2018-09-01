ae9ac51df5ce5796cd50db99b8b6b8386a166b3d
Alex M <alex@belouga.org>
2018-09-01 17:30:45 -0400

    Params update test

--- selfdrive/common/version.h
+++ selfdrive/common/version.h
@@ -1,1 +1,1 @@
-#define COMMA_VERSION "0.5.2-alex6"
+#define COMMA_VERSION "0.5.2-alex7"
--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -232,6 +232,7 @@ def state_control(plan, CS, CP, state, events, v_cruise_kph, v_cruise_kph_last, 
 
   enabled = isEnabled(state)
   active = isActive(state)
+  params = Params()
 
   # check if user has interacted with the car
   driver_engaged = len(CS.buttonEvents) > 0 or \
@@ -245,7 +246,7 @@ def state_control(plan, CS, CP, state, events, v_cruise_kph, v_cruise_kph_last, 
             set_follow_distance += 1
         else:
             set_follow_distance = 0
-        #params.put("CarFollowDistance", str(set_follow_distance))
+        params.put("CarFollowDistance", str(set_follow_distance))
 
 
 #    if b.type == "leftBlinker":
