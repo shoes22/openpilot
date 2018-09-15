34c2b8b4f2a92fff02e1b101f2c35e40ef92abd5
Alex M <alex@belouga.org>
2018-09-15 01:29:36 -0400

    Revert "Go from params to CS for follow_distance"
    This reverts commit 23682ef10ea55bfcaaeabfeab62a9cd560de3707.

--- selfdrive/common/version.h
+++ selfdrive/common/version.h
@@ -1,1 +1,1 @@
-#define COMMA_VERSION "0.5.3-alex12"
+#define COMMA_VERSION "0.5.3-alex11"
--- selfdrive/controls/lib/planner.py
+++ selfdrive/controls/lib/planner.py
@@ -7,6 +7,7 @@ from copy import copy
 from cereal import log
 from collections import defaultdict
 from common.realtime import sec_since_boot
+from common.params import Params
 from common.numpy_fast import interp
 import selfdrive.messaging as messaging
 from selfdrive.swaglog import cloudlog
@@ -23,6 +24,7 @@ _DT = 0.01    # 100Hz
 _DT_MPC = 0.2  # 5Hz
 MAX_SPEED_ERROR = 2.0
 AWARENESS_DECEL = -0.2     # car smoothly decel at .2m/s^2 when user is distracted
+params = Params()
 #TR = 1.4 # CS.distance_toggle
 GPS_PLANNER_ADDR = "192.168.5.1"
 
@@ -219,7 +221,7 @@ class LongitudinalMpc(object):
 
     # Calculate mpc
     t = sec_since_boot()
-    distanceToggle = CS.hud_follow_distance
+    distanceToggle = int(params.get("CarFollowDistance"))
     if distanceToggle == 3:
         self.TR = 1.8
     elif distanceToggle == 2:
