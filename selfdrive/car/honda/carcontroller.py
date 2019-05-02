8c8ba28d9b46964319f1a672d5c2495fe694c3b2
Alex M <alex@belouga.org>
2019-05-01 21:30:05 -0400

    Make sure we send button messages when op is engaged

--- selfdrive/car/honda/carcontroller.py
+++ selfdrive/car/honda/carcontroller.py
@@ -164,7 +164,7 @@ class CarController(object):
     if (frame % 10) == 0:
       idx = (frame/10) % 4
       can_sends.extend(hondacan.create_ui_commands(self.packer, pcm_speed, hud, CS.CP.carFingerprint, idx))
-      if update_speed:
+      if enabled and update_speed:
           can_sends.append(hondacan.spam_buttons_command(self.packer, CruiseButtons.RES_ACCEL, idx))
 
     if CS.CP.radarOffCan:
