9f9942027d729af88f82790e19aeff59abbd76cd
Alex M <alex@belouga.org>
2020-04-06 19:42:04 -0400

    Remove repeat freeSpace

--- selfdrive/ui/ui.hpp
+++ selfdrive/ui/ui.hpp
@@ -148,7 +148,6 @@ typedef struct UIScene {
   uint16_t maxCpuTemp;
   uint32_t maxBatTemp;
   float gpsAccuracy ;
-  float freeSpace;
   float angleSteers;
   float angleSteersDes;
   //BB END CPU TEMP
